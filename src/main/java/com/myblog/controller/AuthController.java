2ef7d4ca003ea06b0aa23e55029e88355f8e5356
Lucky <luckymahanta2001@gmail.com>
2024-02-26 11:52:08 +0530

    25: Created 2 table role and user role, and giv roles , and also use Set collection

--- src/main/java/com/myblog/controller/AuthController.java
+++ src/main/java/com/myblog/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.myblog.controller;
 
+import com.myblog.entity.Role;
 import com.myblog.entity.User;
 import com.myblog.payload.SignUpDto;
+import com.myblog.repository.RoleRepository;
 import com.myblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -19,6 +26,8 @@ public class AuthController {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto){
@@ -33,6 +42,13 @@ public class AuthController {
         user.setUsername(signUpDto.getUsername());
         user.setEmail(signUpDto.getEmail());
         user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = roleRepository.findByName(signUpDto.getRoleType()).get();
+        Set<Role> convert= new HashSet<>();
+        convert.add(roles);
+        user.setRoles(convert);
+//        user.setRoles(Collections.singleton(roles));
+
         userRepository.save(user);
         return new ResponseEntity<>("User Register Successfully", HttpStatus.CREATED);
     }
