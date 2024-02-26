85178fd99e7fa855fa29966bfbfebb6875b83043
Lucky <luckymahanta2001@gmail.com>
2024-02-27 05:04:11 +0530

    25: Spring Security Sign Rest API Implementation

--- src/main/java/com/myblog/controller/AuthController.java
+++ src/main/java/com/myblog/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.myblog.controller;
 
 import com.myblog.entity.Role;
 import com.myblog.entity.User;
+import com.myblog.payload.LoginDto;
 import com.myblog.payload.SignUpDto;
 import com.myblog.repository.RoleRepository;
 import com.myblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,11 +28,23 @@ import java.util.Set;
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginDto loginDto){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsernameOrEmail(),
+                        loginDto.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        return new ResponseEntity<>("User Sign-in successfully!", HttpStatus.OK);
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto){
---
+++ src/main/java/com/myblog/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.myblog.security;
+
+import com.myblog.entity.Role;
+import com.myblog.entity.User;
+import com.myblog.exception.ResourceNotFoundException;
+import com.myblog.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailOrUsername(username, username).orElseThrow(
+                () -> new ResourceNotFoundException("user not found with username or email" + username));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRoleToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRoleToAuthorities(Set<Role> roles) {
+        return roles.stream().map(role ->  new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+}
