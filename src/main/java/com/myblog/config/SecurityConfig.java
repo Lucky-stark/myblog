0d3863dfd9cd88dc1701f21e9058deba6bc1f173
Lucky <luckymahanta2001@gmail.com>
2024-02-26 10:32:00 +0530

    25: Created Signup class and encode password

--- src/main/java/com/myblog/config/SecurityConfig.java
+++ src/main/java/com/myblog/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
